863f0e166ce7fba22cb397391d15bc393bc9000d
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-21 14:38:13 -0400

    Agregando Swagger

--- BlazorEcommerce/BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +24,10 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwaggerUI();
+
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 
