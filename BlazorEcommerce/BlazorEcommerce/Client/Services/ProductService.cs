720874d805c2aa94d7f53df323cc0f4ef673abf7
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-24 09:35:45 -0400

    Use Dependency Injection in the Client

---
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorEcommerce.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient http;
+
+        public ProductService(HttpClient http)
+        {
+            this.http = http;
+        }
+        public static List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var result = await http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            if (result is not null && result.Data is not null) Products = result.Data;
+        }
+    }
+}
